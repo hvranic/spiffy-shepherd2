0da9367823f8105061485309be243ad1c9b0d3d9
hvranic <hvranic@gmail.com>
2024-12-11 12:41:31 +0100

    Final edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://spiffy-shepherd.netlify.app/"
+  site: "https://spiffy-shepherd.netlify.app/",
+  integrations: [preact()]
 });
